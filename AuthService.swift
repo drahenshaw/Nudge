e8a32a81b9e4038836bbd7118b1d0708e2dfa2f4
drahenshaw <drahenshaw@gmail.com>
2019-04-13 09:15:29 -0700

    AuthService & DataServices for Friebase Configuration

---
+++ AuthService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthService.swift
+//  Hope
+//
+//  Created by David Henshaw on 4/13/19.
+//  Copyright © 2019 David Henshaw. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class AuthService
+{
+    static let instance = AuthService()
+    
+    func RegisterUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?)->())
+    {
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.user.providerID, "email": user.user.email]
+            //Data Service here
+            userCreationComplete(true, nil)
+        })
+    }
+    
+    func LoginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?)->())
+    {
+        Auth.auth().signIn(withEmail: email, password: password) { (AuthDataResult, error) in
+            if error != nil {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+
+}
+
---
+++ DataService.swift
@@ -0,0 +1,21 @@
+//
+//  DataService.swift
+//  Hope
+//
+//  Created by David Henshaw on 4/13/19.
+//  Copyright © 2019 David Henshaw. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = ""
+
+class DataService
+{
+    
+    
+    
+    
+    
+}
