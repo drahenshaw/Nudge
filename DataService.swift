33babafa5e25bc2d188dfc9aaad553230a16d5e8
drahenshaw <drahenshaw@gmail.com>
2019-04-13 09:32:28 -0700

    DataService class with default methods

--- DataService.swift
+++ DataService.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 import Firebase
+import FirebaseDatabase
 
-let DB_BASE = ""
+let DB_BASE_REF = Database.database().reference()
+
 
 class DataService
 {
+    // Static Instance to Firebase Reference
+    static let instance = DataService()
     
+    // Private Versions of DB Children
+    private var _REF_BASE = DB_BASE_REF
+    private var _REF_USERS = DB_BASE_REF.child("users")
     
+    // Property Versions for DB Children
+    var REF_BASE: DatabaseReference
+    {
+        return _REF_BASE
+    }
     
+    var REF_USERS: DatabaseReference
+    {
+        return _REF_USERS
+    }
     
+    // Create Firebase User with User ID, Dictionary <String, Data>
+    // Hashable by User ID, Data includes all properties for a user
+    func CreateDBUser(uid: String, userData: Dictionary<String, Any>)
+    {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    // Data Service Methods
     
 }
