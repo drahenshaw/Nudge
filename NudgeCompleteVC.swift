93f1eec62e061e19aa28545b87563abd1799b64b
drahenshaw <drahenshaw@gmail.com>
2019-04-13 17:17:58 -0700

    Nudge complete screen and final app connections. Skeleton nearly complete

--- NudgeCompleteVC.swift
+++ NudgeCompleteVC.swift
@@ -16,15 +16,11 @@ class NudgeCompleteVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func NextNudgePressed(_ sender: Any) {
+        performSegue(withIdentifier: "ToNextNudge", sender: self)
     }
-    */
-
+    
+    @IBAction func DoneButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "ToFinished", sender: self)
+    }
 }
